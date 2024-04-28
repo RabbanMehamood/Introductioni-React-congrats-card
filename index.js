15804fa7d0b60f3482c1ef6bedfe8f9ac765861c
RabbanMehamood <shaik.mehamood786@gmail.com>
2024-04-28 15:19:23 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  // Write your code here.
+  <div class='bg-container'>
+    <h1>Congratulations</h1>
+    <div class='card-container'>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png'
+        class='image'
+      />
+      <h3>Kiran V</h3>
+      <p>Vishnu Istittute of TEchnology , bhimavaram</p>
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
